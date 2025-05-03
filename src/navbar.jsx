39b8b6367609f2b2d9970c8ac8609183aa1532d3
gokul810 <gk6790655@gmail.com>
2025-05-03 16:57:54 +0530

    updated ui

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,28 +5,26 @@ import { Home, Settings, StickyNote } from "lucide-react";
 const Navbar = ({ onTabChange }) => {
     return (
         <div className="navbar">
-
             <div className="tooltip">
-            <button className="nav-item" onClick={() => onTabChange("home")}>
-                < Home size={20}/>
-            </button>
-            <span className="tooltip-text">Home</span>
+                <button className="nav-item" onClick={() => onTabChange("home")}>
+                    <Home size={20} />
+                </button>
+                <span className="tooltip-text">Home</span>
             </div>
 
             <div className="tooltip">
-            <button className="nav-item" onClick={() => onTabChange("notes")}>
-                < StickyNote size={20}/>
-            </button>
-            <span className="tooltip-text">Notes</span>
-            </div>
-            
-            <div className="tooltip">
-            <button className="nav-item" onClick={() => onTabChange("settings")}>
-                < Settings size={20}/>
-            </button>
-            <span className="tooltip-text">Settings</span>
+                <button className="nav-item" onClick={() => onTabChange("notes")}>
+                    <StickyNote size={20} />
+                </button>
+                <span className="tooltip-text">Notes</span>
             </div>
 
+            <div className="tooltip">
+                <button className="nav-item" onClick={() => onTabChange("settings")}>
+                    <Settings size={20} />
+                </button>
+                <span className="tooltip-text">Settings</span>
+            </div>
         </div>
     );
 };
--- src/tabs/Home.jsx
+++ src/tabs/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "../../style/home.css";
 
+let inputValue;
+
 const Home = () => {
 
     return (
         <div className="search-container">
-            <div className="profile">
-                <p>G</p>
-            </div>
+            <h1 style={{fontSize: "45px"}}>Lynk Notes</h1>
             <input
                 type="text"
                 placeholder="Search YouTube or paste a link..."
+                value={inputValue}
                 className="search-input"
             />
         </div>
